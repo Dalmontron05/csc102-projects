0cf669fc88e58297dbae314e5ba7c54a451c3656
Dalmontron05 <vexr2dts85338@gmail.com>
2023-11-23 12:12:30 -0700

    finished initial array; cleaned out old flashcard js

--- r6-operators/main.js
+++ r6-operators/main.js
@@ -16,83 +16,22 @@ const operators =
     {
         operatorName: "Smoke",
         operatorSpeed: 2,
-        operatorQuote: "Smokes Quote placeholder",
+        operatorQuote: "\"Sure, I care about the enviroment. Immediately around me, anyway.\"",
         operatorDescription: "Smokes description placeholder",
-        operatorPortrait: "smokes portrait placeholder",
-        operatorIcon: "smokes icon placeholder"
+        operatorPortrait: "https://static.wikia.nocookie.net/rainbowsix/images/0/0d/Smoke_-_Full_Body.png",
+        operatorIcon: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6mOMxoc3t27R9Gr9wFtt6T/f5f90a6be6a6710bee686ce69df29906/Y0R6_BADGE_Smoke_L.png"
     },
     {
-        // put a 3 speed here
+        operatorName: "Bandit",
+        operatorSpeed: 3,
+        operatorQuote: "\"Survival is all about timing. Nature doesn’t give second chances.\"",
+        operatorDescription: "Bandits placeholder description",
+        operatorPortrait: "https://static.wikia.nocookie.net/rainbowsix/images/0/0a/Large-bandit.463ee829.png",
+        operatorIcon: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/4Vasjq82J1TNjNE38J7LmV/e1725f8d9ea2b85565e40b54faac85b8/Y0R6_BADGE_Bandit_L.png"
     }
 ]
 
 
-
-
-const flashcards = 
-[
-    {
-        question: 'What is the capitial of the United States?',
-        answer: 'Washington, D.C.'
-    },
-    {
-        question: 'What is 2+2?',
-        answer: '4'
-    },
-    { question: 'who is the best op in siege?', answer: 'tachanka'}
-];
-
-
-// Create a variable to store the current flashcard index
-let currentCard = 0;
-
-
-// Create a function to update the flashcards
-function updateFlashCard()
-{
-    // get the current card object
-    const card = document.getElementById('flashcard');
-    
-
-    // Check if the card is flipped in case flipped means the answer is showing. There are only two sides to a flash card. The question side and the answer side.
-    if(card.classList.contains('flipped'))
-    {
-        card.textContent = flashcards[currentCard].answer;
-    }
-    else
-    {
-        card.textContent = flashcards[currentCard].question;
-    }
-}
-
-
-// init first card
-updateFlashCard();
-
-
-// This is the event listener for the flash card
-document.getElementById('flashcard').addEventListener('click', function()
-{
-    // Toggle the flipped class so the card flips
-    this.classList.toggle('flipped');
-
-    // Update the card content by calling the updateFlashCard function
-    updateFlashCard();
-});
-
-
-// Event listener for next button
-document.getElementById('next').addEventListener('click', function()
-{
-    // Increment the current card index
-    currentCard = (currentCard + 1) % flashcards.length;
-
-    // Get the flash card element
-    const card = document.getElementById('flashcard');
-
-    // Remove...
-    card.classList.remove('flipped');
-    
-    updateFlashCard();
-});
+// Create a variable to store the current operator index
+let currentOperator = 0;
 
